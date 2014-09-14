cf8a3290213eb1df427dac7ec077aea2e2e8b295
Paolo Montrasio <paolo@paolomontrasio.com>
2014-09-14 17:58:02 +0200

    Accepting login parameters

--- web/controllers/sessions_controller.ex
+++ web/controllers/sessions_controller.ex
@@ -1,12 +1,20 @@
 defmodule MyProject.SessionsController do
   use Phoenix.Controller
+	alias MyProject.Router # So we can just write Router.
+	require Logger
 
   def new(conn, _params) do
     render conn, "new"
   end
 
+	# This will work only if it receives user[email] and user[password]
+	# If one of them is missing we'll get an error
+	# (FunctionClauseError) no function clause matching in MyProject.SessionsController.create/2
   def create(conn, _params) do
-    render conn, "create"
+		%{ "user" => %{ "email" => email, "password" => password }} = _params
+		Logger.debug "EMAIL #{email}, PASSWORD #{password}"
+
+    redirect conn, Router.pages_path(:index)
   end
 
   def destroy(conn, _params) do

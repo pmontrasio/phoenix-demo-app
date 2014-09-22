1e976e2f200de0be3e7755e8040e1859c7b3f0c8
Paolo Montrasio <paolo@paolomontrasio.com>
2014-09-22 23:41:24 +0200

    Added authentication on websockets

--- web/channels/chat_channel.ex
+++ web/channels/chat_channel.ex
@@ -1,30 +1,58 @@
 defmodule DemoApp.ChatChannel do
   use Phoenix.Channel
+	import Plug.Conn
 	require Logger
+	alias DemoApp.User
 
-	# Note: I'm not doing checks on the authentication state of the
-	# users so this chat is public. This is probably not what you usually
-	# want because anybody can write its own program to connect to the
-	# public websocket of the chat and tap into it.
+	# User authentication on websockets is the subject of this issue
+	# https://github.com/phoenixframework/phoenix/issues/218
 
   def join(socket, topic, message) do
-		user = message["user"]
-		#Logger.debug "JOIN #{user}"
-		broadcast socket, "join", %{content: "#{user} joined the chat"}
-    {:ok, socket}
-  end
+		#Logger.debug "JOIN?"
+		user = get_user(socket)
+		if user == nil do
+			{:error, socket, "Not logged in"}
+		else
+			#Logger.debug "JOIN #{user.email}"
+			broadcast socket, "join", %{content: "#{user.email} joined the chat"}
+			{:ok, socket}
+		end
+	end
 
   def event(socket, "chat:message", message) do
-		#Logger.debug "#{message["user"]}: #{message["message"]}"
-		broadcast socket, "message", message
+		user = get_user(socket)
+		unless user == nil do
+			#Logger.debug "MESSAGE #{user.email}: #{message["message"]}"
+			broadcast socket, "message", message
+		end
 		socket
 	end
 
+	# This is called both when the JavaScript sends a "leave" event
+	# and when the page with the websocket is closed.
 	def leave(socket, topic) do
-		#Logger.debug "SOMEBODY LEFT"
-		broadcast socket, "user:left", %{ "content" => "somebody left the chat" }
-		# next step: identify who left the chat. Check Socket.assigns and Socket.conn
+		#Logger.debug "LEAVE"
+		user = get_user(socket)
+		unless user == nil do
+			broadcast socket, "user:left", %{ "content" => "#{user.email} left the chat" }
+		end
 		socket
+	end
+
+	defp get_user(socket) do
+		{:ok, %{user_id: user_id}} = get_session(socket)
+		unless user_id == nil do
+			Repo.get(User, user_id)
+		else
+			nil
+		end
+	end
+
+	defp get_session(socket) do
+		{"cookie", s1} = List.keyfind(elem(socket.conn, 16), "cookie", 0)
+		s2 = Plug.Conn.Cookies.decode(s1)
+	  %{"_demo_app_key" => secret} = s2
+		Plug.MessageVerifier.verify "4D5JB(FQM+@YEVCN0D#O$V%__R&N78G=^J$)_TE*L+7BLNLJUMTI&6Y6T6JML^%UPM&LN%67LW=*)S", secret
 	end
 
 end
